ef0a89e4058fd8d4375649af1aabfbbe1207daa3
mitou <xmitoux@gmail.com>
2025-02-12 22:46:38 +0900

    🎨 design: フォント設定

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { kosugi_maru } from '@repo/ui/utils';
+
 export default function Document() {
   return (
-    <Html lang="ja" suppressHydrationWarning>
+    <Html className={kosugi_maru.className} lang="ja" suppressHydrationWarning>
       <Head>
         <link crossOrigin="use-credentials" href="/manifest.json" rel="manifest" />
         <link href="/apple-touch-icon-180x180.png" rel="apple-touch-icon"></link>
