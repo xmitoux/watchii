a275310f32c07bef9baacadbf4ddf1c3ffed8a8f
mitou <xmitoux@gmail.com>
2025-03-18 23:04:17 +0900

    ✨ feat: Post一覧に投稿日時を表示する

--- apps/frontend/src/components/PostGallery/PostGallery.tsx
+++ apps/frontend/src/components/PostGallery/PostGallery.tsx
@@ -1,33 +1,37 @@
 // post一覧コンポーネント
 
-import { Box, Flex } from '@repo/ui/chakra-ui';
+import { Flex, Text } from '@repo/ui/chakra-ui';
 import { NextImage } from '@repo/ui/components';
-import { SimplePost } from '@repo/ui/types';
+import { formatToJST } from '@repo/ui/utils';
 
 import { usePostImageWidth } from '@/hooks/usePostImageWidth';
+import { PostEntity } from '@/types/post-types';
 
 type PostGalleryProps = {
-  posts: SimplePost[];
+  posts: PostEntity[];
 };
 
 export const PostGallery = ({ posts }: PostGalleryProps) => {
   const imageWidth = usePostImageWidth();
 
   return (
-    <>
-      <Flex justify="center" gap={4} flexWrap="wrap">
-        {posts.map((post) => (
-          <Box key={post.id}>
-            <NextImage
-              src={post.filename}
-              width={400}
-              styleWidth={imageWidth}
-              alt={post.filename}
-              priority
-            />
-          </Box>
-        ))}
-      </Flex>
-    </>
+    <Flex justify="center" gap={4} flexWrap="wrap">
+      {posts.map((post) => (
+        <Flex key={post.id} direction="column" align="end" gap={1}>
+          <NextImage
+            src={post.filename}
+            width={400}
+            styleWidth={imageWidth}
+            alt={post.filename}
+            priority
+          />
+          <Text fontSize="sm" color="gray">
+            投稿:
+            {' '}
+            {formatToJST(post.postedAt)}
+          </Text>
+        </Flex>
+      ))}
+    </Flex>
   );
 };
