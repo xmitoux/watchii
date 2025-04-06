ce9da2ce862c1b7b5dc95c7d4f77ee5af8b78a44
mitou <xmitoux@gmail.com>
2025-04-06 10:01:38 +0900

    🔧 fix: ui packageでESLintが動いていなかったので修正
    - front, adminと同じにした
    - backendは動いてないかも…

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,3 +1,20 @@
-import { eslintConfigCustom } from '@repo/eslint-config';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export default [...eslintConfigCustom];
+import { FlatCompat } from '@eslint/eslintrc';
+
+import { eslintConfigForNext } from '@repo/eslint-config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...eslintConfigForNext,
+];
+
+export default eslintConfig;
