d50e869f20fdbd5eddab59f61ec07f9a7e13db5f
mitou <xmitoux@gmail.com>
2025-04-26 11:22:37 +0900

    🚧 update: 語録作成画面をSSGにする
    - コメント参照

--- apps/admin/src/pages/tags/popular-word/create.tsx
+++ apps/admin/src/pages/tags/popular-word/create.tsx
@@ -1,10 +1,10 @@
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, GetStaticProps } from 'next';
 
 import { tagsApi } from '@/features/Tags/api/tags-api';
 import PopularWordCreate from '@/features/Tags/PopularWord/PopularWordCreate/PopularWordCreate';
 import { PopularWordCreateProps } from '@/features/Tags/types/tags-types';
 
-export const getServerSideProps: GetServerSideProps<PopularWordCreateProps> = async () => {
+export const getStaticProps: GetStaticProps<PopularWordCreateProps> = async () => {
   try {
     // キャラクター一覧をAPIから取得
     const { characters } = await tagsApi.findAllCharacters();
@@ -24,6 +24,28 @@ export const getServerSideProps: GetServerSideProps<PopularWordCreateProps> = as
     };
   }
 };
+
+// TODO: SSRなのにキャラクターが取得されない問題があるのでとりあえずSSGにしておく
+// export const getServerSideProps: GetServerSideProps<PopularWordCreateProps> = async () => {
+//   try {
+//     // キャラクター一覧をAPIから取得
+//     const { characters } = await tagsApi.findAllCharacters();
+
+//     // ページコンポーネントに渡すpropsを返す
+//     return {
+//       props: {
+//         characters,
+//       },
+//     };
+//   }
+//   catch (error) {
+//     console.error('語録登録ページ生成処理に失敗しました。', error);
+//     // エラーが発生した場合は404ページを表示
+//     return {
+//       notFound: true,
+//     };
+//   }
+// };
 
 export default function PopularWordCreatePage({ characters }: PopularWordCreateProps) {
   return <PopularWordCreate characters={characters} />;
