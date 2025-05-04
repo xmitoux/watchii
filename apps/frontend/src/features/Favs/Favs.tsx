359f0cac5de6e426f1f5b85c4f0e2b74723dbb23
mitou <xmitoux@gmail.com>
2025-05-04 23:58:25 +0900

    ✨ udpate: お気に入り一覧ページをSSRではなくCSRにする
    - お気に入りボタンで使っているカスタムフックがSWRを使っているので共用すればSSRより速いはず

--- apps/frontend/src/features/Favs/Favs.tsx
+++ apps/frontend/src/features/Favs/Favs.tsx
@@ -7,11 +7,21 @@ import { usePagination } from '@/components/Pagination/hooks/usePagination';
 import { PostGallery } from '@/components/PostGallery/PostGallery';
 import { useLayoutScroll } from '@/hooks/useLayoutScroll';
 import { useNavigationRestore } from '@/hooks/useNavigationRestore';
+import { useUserFavs } from '@/hooks/useUserFavs';
 import { useFavsStore } from '@/stores/favsStore';
 
 import { FavsProps } from './types/favs-types';
 
-export default function Favs({ posts, total, currentPage, perPage }: FavsProps) {
+export default function Favs({ currentPage, perPage }: FavsProps) {
+  const { favPosts, isFavLoading } = useUserFavs();
+  const total = favPosts.length;
+
+  // 現在のページと表示件数に基づいてデータをオフセット
+  const startIndex = (currentPage - 1) * perPage;
+  const endIndex = startIndex + perPage;
+  // 現在のページに表示するデータを切り出す
+  const posts = favPosts.slice(startIndex, endIndex);
+
   const { scrollRef } = useLayoutScroll();
 
   const { pagination } = usePagination({
@@ -41,28 +51,30 @@ export default function Favs({ posts, total, currentPage, perPage }: FavsProps) 
       noMenu
       onNavigationBack={handleBack}
     >
-      {total === 0
-        ? (
-          <MessageWithImage
-            title="お気に入りがないよ！"
-            messages="漫画詳細から追加してみよう！"
-            imageSrc="/images/no-favs.webp"
-          />
-        )
-        : (
-          <>
-            {/* post一覧 */}
-            <PostGallery posts={posts} />
-
-            {/* Postページシャトル */}
-            <PostPageShuttle
-              scrollRef={scrollRef}
-              postsPerPage={posts.length}
-              postsTotal={total}
-              pageOffset={currentPage * perPage - perPage}
+      {isFavLoading
+        ? <div>Loading...</div>
+        : total === 0
+          ? (
+            <MessageWithImage
+              title="お気に入りがないよ"
+              messages="漫画詳細から追加してみよう！"
+              imageSrc="/images/no-favs.webp"
             />
-          </>
-        )}
+          )
+          : (
+            <>
+              {/* post一覧 */}
+              <PostGallery posts={posts} />
+
+              {/* Postページシャトル */}
+              <PostPageShuttle
+                scrollRef={scrollRef}
+                postsPerPage={posts.length}
+                postsTotal={total}
+                pageOffset={currentPage * perPage - perPage}
+              />
+            </>
+          )}
     </Layout>
   );
 }
